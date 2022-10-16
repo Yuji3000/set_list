6a3742757de873d9de6faaa804012a862a9b459a
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-10-16 11:35:25 -0600

    add artist new

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -17,6 +17,38 @@ RSpec.describe Artist, type: :model do
         expect(@prince.average_song_length.round(2)).to eq(424.67)
       
       end
+      
+      describe '#songs' do 
+        it 'returns all of the associated song objects' do
+          # First, comment out the has_many :songs Association from the Artist model
+          # Then, make this test pass without using Associations
+          
+          expect(@prince.songs).to eq([@purple, @beret, @other_song])
+        end
+      end
+
+      describe '#song_count' do
+        it 'counts the number of songs' do
+          # Make this test pass WITH Associations
+          expect(@prince.song_count).to eq(3)
+        end
+      end
+
+      describe '#wrote_song?' do
+        #write two versions of this method
+        # Version 1 - Make these tests pass WITH Association
+        # Version 2 - Make these tests pass WITHOUT Associations
+
+        it 'returns true if the song is associated with the artist' do
+          expect(@prince.wrote_song?(@purple)).to be (true)
+        end
+
+        it 'returns false if the song is not associated with the artist' do
+          diff_artist = Artist.create!(name: 'Different Artist')
+          diff_song = diff_artist.songs.create!(title: 'Different Song', length: 0, play_count: 0)
+          expect(@prince.wrote_song?(diff_song)).to be(false)
+        end
+      end
     end
   end
 end
